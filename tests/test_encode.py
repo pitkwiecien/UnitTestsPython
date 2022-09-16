ec63dfa3bb4e75a7ccba2024b34d163672cbd22a
pitkw <pitkwiecien@gmail.com>
2022-09-17 00:12:10 +0200

    Added documentation to test_encode.py

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -4,18 +4,34 @@ from main import encode
 
 class TestEncode(TestCase):
     def test_primary_cypher(self):
+        """
+        Sprawdza czy przekształcanie w pierwszą stronę działa
+        """
+
         self.assertEqual(encode("GBC"), "ABC")
         self.assertEqual(encode("DPL"), "EOU")
 
     def test_inverted_cypher(self):
+        """
+        Sprawdza czy przekształcanie w drugą stronę działa
+        """
+
         self.assertEqual(encode("AYE"), "GRD")
         self.assertEqual(encode("OUI"), "PLK")
 
     def test_mixed_cyphers(self):
+        """
+        Sprawdza czy przekształcanie w obie strony w jednym tekście działa
+        """
+
         self.assertEqual(encode("ADR"), "GEY")
         self.assertEqual(encode("PROGRAM"), "OYPAYGM")
 
     def test_small_letters(self):
+        """
+        Sprawdza czy małe litery pozostają niezmienne
+        """
+
         self.assertEqual(encode("gbc"), "gbc")
         self.assertEqual(encode("dpl"), "dpl")
         self.assertEqual(encode("aye"), "aye")
