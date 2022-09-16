2fd4806b757597898d445fd634282f98e6526634
pitkw <pitkwiecien@gmail.com>
2022-09-17 00:21:02 +0200

    Changed docstring to double quotes in main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def encode(text: str) -> str:
-    '''
+    """
     :param str text: The text which we want to encode
     :return: The encoded text
     :rtype: str
-    '''
+    """
 
     cypher = {"G": "A", "D": "E", "R": "Y", "P": "O", "L": "U", "K": "I"}
-    inv_cypher = {value : key for key, value in cypher.items()}
+    inv_cypher = {value: key for key, value in cypher.items()}
     new_text = ""
     for letter in text:
         if letter in cypher.keys():
